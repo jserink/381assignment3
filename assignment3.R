caf7e972c2f6163d6234f74ea966c743558c5ddb
rich <rich.p.martin@gmail.com>
2020-10-14 10:16:23 -0700

    think it is good to go...

--- assignment3.R
+++ assignment3.R
@@ -1,15 +1,17 @@
 ###### This is the file where you save your R code that produces the objects (graphs, tables) that you want to embed into 
 # your assignment.  Make sure that there are no errors when you source this file before sourcing it from assignment3.Rmd file.
-# To show you how it is done I have included code below that answers questions 1 and 2.
+# To show you how it is done I have included code below that answers question 3.
 
 rm(list=ls()) #makes sure that your work environment is clean.
 library("tidyverse") #we use functions from this library.
 
-######Question 3
-
 mydf <- read_csv("publicdata/wtpvswta1.csv") # reads in the data
 mydf$strategy <- factor(mydf$strategy,labels=c("control","hints")) # strategy is coded 0/1: instead give it meaningful names.
 mydf$size <- factor(mydf$size,labels=c("small","large")) # size is coded 5/20: give it meaningful names.
+mydf <- mydf%>%
+  mutate(endowment_effect=ask-bid)
+
+######Question 3
 
 (first_plot <- mydf %>% # assign the name first_plot (referenced in your assignment3.Rmd file), use dataframe mydf THEN
   ggplot(aes(x=factor(size),y=bid,fill=strategy))+ # x axis is group size, y axis is bids, with separate boxes for strategies.
