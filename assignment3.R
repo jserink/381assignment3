e98335967829736d410f88a1b3110a61ff36cc6d
rich <rich.p.martin@gmail.com>
2020-10-15 20:06:55 -0700

    econ 381 assignment 3: Fall 2020

--- assignment3.R
+++ assignment3.R
@@ -1,17 +1,16 @@
 ###### This is the file where you save your R code that produces the objects (graphs, tables) that you want to embed into 
 # your assignment.  Make sure that there are no errors when you source this file before sourcing it from assignment3.Rmd file.
-# To show you how it is done I have included code below that answers question 3.
+# To show you how it is done I have included code below that answers questions 1 and 2.
 
 rm(list=ls()) #makes sure that your work environment is clean.
 library("tidyverse") #we use functions from this library.
-slash <- ifelse(.Platform$OS.type=="unix", "/", "\\") #The eternal directory battle: Windows vs. *nix
 
 ####################which section?
 section <- 1
 #########################
-mydf <- read_csv(paste("publicdata",section,slash,"wtpvswta",section,".csv",sep="")) # reads in the data
-mydf$strategy <- factor(mydf$strategy,labels=c("control","hints")) # strategy is coded 0/1: instead give it meaningful names.
-mydf$size <- factor(mydf$size,labels=c("small","large")) # size is coded 5/20: give it meaningful names.
+mydf <- read_csv(paste("publicdata",section,"/wtpvswta",section,".csv",sep="")) # reads in the data
+mydf$strategy <- factor(mydf$strategy,labels=c("no hints","hints")) # strategy is coded 0/1: instead give it meaningful names.
+mydf$size <- factor(mydf$size,labels=c("group of 5","group of 20")) # size is coded 5/20: give it meaningful names.
 mydf <- mydf%>%
   mutate(endowment_effect=ask-bid)
 
@@ -22,4 +21,5 @@ mydf <- mydf%>%
   geom_boxplot(outlier.shape = NA)+# do not plot outliers because we add data below.
   geom_point(position=position_jitterdodge(),alpha=0.3) + # the data with some random noise and transparency to avoid overplotting.
   labs(y="bids", x="subsequent group size",fill="strategy manipulation")) # some labels.
+
 
